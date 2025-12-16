85c90f924d5be79d15e0370856bc2c4c05a6729b
Léo Brucker <uhugu@student.kit.edu>
2025-12-16 12:59:38 +0100

    Pass lecture 6

--- lecture_06_distribution_given_player_cable/exercise.py
+++ lecture_06_distribution_given_player_cable/exercise.py
@@ -11,6 +11,21 @@ try:
 except (ImportError, NotImplementedError):
     from lecture_03_position_probability.solution import position_probability_given_cables
 
+def starting_cables(N: int, M: int) -> list[int]:
+    cables = []
+    for i in range(N):
+        for _ in range(M):
+            cables.append(i+1)
+    return cables
+
+def smaller_numbers_count(cables: list[int], i: int) -> int:
+    cables.sort()
+    count = 0
+    for number in cables:
+        if number == i:
+            break
+        count += 1
+    return count
 
 def exact_distribution_given_player_cables(P: int, N: int, M: int, player_cables: list[int]) -> list[list[float]]:
     """
@@ -35,4 +50,29 @@ def exact_distribution_given_player_cables(P: int, N: int, M: int, player_cables
         dist = exact_distribution_given_player_cables(4, 4, 4, [1, 1, 2, 3])
         # dist[0][0] should be the probability that number 1 appears at position 0 for player 1
     """
-    raise NotImplementedError("exact_distribution_given_player_cables not yet implemented")
+    distribution : list[list[float]] = []
+    cables = starting_cables(N, M)
+    print(f"Cables before: {cables}")
+    for player_cable in player_cables:
+        cables.remove(player_cable)
+    print(f"Cables after: {cables}")
+    P = P - 1 # one player already got his cables
+    T = len(cables) # total cables
+
+    c = int(T / P) # cables per player
+    E = T % P # number of players with c + 1 cables
+    max_positions = c if E == 0 else c+1
+    # loop through all numbers
+    for i in range(N):
+        positions_probs: list[float] = []
+        M = cables.count(i+1) # i+1 cause numbers are 1-indexed
+        smaller_numbers = smaller_numbers_count(cables, i+1)
+        # loop through all positions
+        for j in range(max_positions):
+            prob_C = position_probability_given_cables(M, T, c, j, smaller_numbers)
+            prob_E = position_probability_given_cables(M, T, c+1, j, smaller_numbers)
+            prob = ((P-E)/P) * prob_C + (E/P) * prob_E
+            positions_probs.append(prob)
+        distribution.append(positions_probs)
+    return distribution
+
